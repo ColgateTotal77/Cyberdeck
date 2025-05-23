16f5ab981880556602ca4c864f207ac46448aa4e
MSTWNTED <den7653090191@gmail.com>
2025-05-23 20:08:07 +0200

    changed card names in database

--- db.sql
+++ db.sql
@@ -39,26 +39,26 @@ CREATE TABLE IF NOT EXISTS cards (
 );
 
 INSERT INTO cards (id, name, description, attack, hp, cost) VALUES
-(1, 'Iron Man', 'Fires repulsor blasts from his suit.', 7, 5, 4),
-(2, 'Captain America', 'Blocks attacks with his vibranium shield.', 5, 8, 3),
-(3, 'Thor', 'Strikes enemies with Mjolnir.', 9, 6, 5),
-(4, 'Hulk', 'Smashes everything in sight.', 10, 7, 6),
-(5, 'Black Widow', 'Fast and agile spy with deadly precision.', 6, 4, 3),
-(6, 'Hawkeye', 'Shoots explosive arrows from a distance.', 5, 3, 2),
-(7, 'Spider-Man', 'Uses webs to trap enemies and move quickly.', 6, 5, 3),
-(8, 'Doctor Strange', 'Uses mystical spells to manipulate the battlefield.', 8, 4, 5),
-(9, 'Black Panther', 'Uses claws and agility to fight fiercely.', 7, 6, 4),
-(10, 'Scarlet Witch', 'Distorts reality and damages multiple enemies.', 9, 5, 5),
-(11, 'Vision', 'Passes through enemies and fires energy beams.', 7, 7, 4),
-(12, 'Ant-Man', 'Shrinks to dodge and surprise enemies.', 5, 5, 3),
-(13, 'Wasp', 'Small, fast, and delivers precise stings.', 4, 4, 2),
-(14, 'Falcon', 'Attacks from above using wings and tech.', 6, 4, 3),
-(15, 'Winter Soldier', 'Expert fighter with a bionic arm.', 7, 5, 4),
-(16, 'Captain Marvel', 'Unleashes cosmic energy blasts.', 9, 6, 5),
-(17, 'Loki', 'Tricks and weakens enemies with illusions.', 5, 5, 3),
-(18, 'Thanos', 'Overwhelming power. Can wipe out all at once.', 10, 10, 7),
-(19, 'Gamora', 'Elite assassin trained by Thanos.', 7, 5, 4),
-(20, 'Star-Lord', 'Quick shooter and unpredictable fighter.', 6, 4, 3);
+(1, 'V', 'Fires repulsor blasts from his suit.', 7, 5, 4),
+(2, 'Johnny Silverhand', 'Blocks attacks with his vibranium shield.', 5, 8, 3),
+(3, 'Panam Palmer', 'Strikes enemies with Mjolnir.', 9, 6, 5),
+(4, 'Viktor Vector', 'Smashes everything in sight.', 10, 7, 6),
+(5, 'Judy Alvarez', 'Fast and agile spy with deadly precision.', 6, 4, 3),
+(6, 'Jackie Welles', 'Shoots explosive arrows from a distance.', 5, 3, 2),
+(7, 'Takemura Goro', 'Uses webs to trap enemies and move quickly.', 6, 5, 3),
+(8, 'Adam Smasher', 'Uses mystical spells to manipulate the battlefield.', 8, 4, 5),
+(9, 'Dexter DeShawn', 'Uses claws and agility to fight fiercely.', 7, 6, 4),
+(10, 'River Ward', 'Distorts reality and damages multiple enemies.', 9, 5, 5),
+(11, 'Evelyn Parker', 'Passes through enemies and fires energy beams.', 7, 7, 4),
+(12, 'Yorinobu Arasaka', 'Shrinks to dodge and surprise enemies.', 5, 5, 3),
+(13, 'Hanako Arasaka', 'Small, fast, and delivers precise stings.', 4, 4, 2),
+(14, 'Placide', 'Attacks from above using wings and tech.', 6, 4, 3),
+(15, 'Maman Brigitte', 'Expert fighter with a bionic arm.', 7, 5, 4),
+(16, 'Kerry Eurodyne', 'Unleashes cosmic energy blasts.', 9, 6, 5),
+(17, 'Rogue Amendiares', 'Tricks and weakens enemies with illusions.', 5, 5, 3),
+(18, 'Delamain', 'Overwhelming power. Can wipe out all at once.', 10, 10, 7),
+(19, 'Meredith Stout', 'Elite assassin trained by Thanos.', 7, 5, 4),
+(20, 'T-Bug', 'Quick shooter and unpredictable fighter.', 6, 4, 3);
 
 -- DROP DATABASE Race01;
 -- DROP USER 'snazarenko'@'localhost';
